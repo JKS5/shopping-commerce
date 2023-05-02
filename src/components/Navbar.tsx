d3ec073616be850d7fe527b8ca564d8cef82f19b
JKS5 <jokekhanson@gmail.com>
2023-05-02 20:46:21 +0900

    firebase

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,18 +2,45 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { FiShoppingBag } from "react-icons/fi";
 import { BsFillPencilFill } from "react-icons/bs";
+// fireBase
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const firebaseConfig = {
+  //...
+};
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    const user = userCredential.user;
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+
+const db = getFirestore(app);
+
+// async function getCities(db) {
+//   const citiesCol = collection(db, "cities");
+//   const citySnapshot = await getDocs(citiesCol);
+//   const cityList = citySnapshot.docs.map((doc) => DocumentReference.data());
+//   return cityList;
+// }
 
 export default function Navbar() {
   return (
-    <header>
-      <Link to="/">
+    <header className="flex justify-between border-b border-gray-300 p-2">
+      <Link to="/" className="flex items-center text-4xl text-brand">
         <FiShoppingBag />
         <h1>Shoppy</h1>
       </Link>
-      <nav>
+      <nav className="flex items-center gap-4 font-semibold">
         <Link to="/products">Products</Link>
         <Link to="/carts">Carts</Link>
-        <Link to="/products/new">
+        <Link to="/products/new" className="text-2xl">
           <BsFillPencilFill />
         </Link>
         <p>로그인</p>
