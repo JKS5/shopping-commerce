c92050ad892569802800a21b8c5d774e5d98d8af
JKS5 <jokekhanson@gmail.com>
2023-05-08 19:05:05 +0900

    UI

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+interface Props {
+  text: string;
+  onClick: () => void;
+}
+
+export default function Button({ text, onClick }: Props) {
+  return (
+    <div
+      className="bg-brand py-2 px-4 text-white rounded-sm hover:brightness-110"
+      onClick={onClick}
+    >
+      {text}
+    </div>
+  );
+}
