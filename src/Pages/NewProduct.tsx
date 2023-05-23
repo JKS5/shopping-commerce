935559d3c3d1823de9a331dabc6053cfe62defdc
JKS5 <jokekhanson@gmail.com>
2023-05-23 21:23:41 +0900

    feat: Added Basic Frame for NewProduct Bussiness-Logic

--- src/Pages/NewProduct.tsx
+++ src/Pages/NewProduct.tsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function NewProduct() {
+  const [files, setFiles] = useState<any>();
+  const [product, setProducts] = useState<any>();
+  const onChange = (e: any) => {
+    const { name, value, files } = e.target;
+    if (name === "file") {
+      setFiles(files);
+      return;
+    }
+    setProducts(() => ({ ...product, [name]: value }));
+  };
+  const onSubmit = (e: any) => {
+    e.preventDefault();
+    //cloudinary로 product를 전송
+  };
   return (
-    <div>
-      <h1>새로운 제품등록</h1>
-    </div>
+    <section>
+      <form onSubmit={onSubmit}>
+        {files && <img src={URL.createObjectURL(files[0])} alt="preview" />}
+        <input
+          type="file"
+          accept="image/*"
+          name="file"
+          onChange={onChange}
+          required
+        />
+        <button>click it</button>
+      </form>
+    </section>
   );
 }
