69807354fcdec9bc3eee3a5437caad4991d97330
JKS5 <jokekhanson@gmail.com>
2023-06-08 13:16:02 +0900

    [hotfix]-Tailwind의 className와JS를 결합했을때, UI가 Network에 제대로 Load되지 않음. 직접바꿈

--- src/components/Home/Banner.tsx
+++ src/components/Home/Banner.tsx
@@ -2,32 +2,42 @@ import React, { useState } from "react";
 import { IoIosArrowRoundBack, IoIosArrowRoundForward } from "react-icons/io";
 import ChangeUiPic from "./ChangeUiPic";
 export default function Banner() {
-  const [currentIndex, setCurrentIndex] = useState(1);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   const prevSlide = () => {
-    const isFirstSlide = currentIndex === 1;
-    const newIndex = isFirstSlide ? 3 : currentIndex - 1;
+    const isFirstSlide = currentIndex === 0;
+    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1;
     setCurrentIndex(newIndex);
   };
   const nextSlide = () => {
-    const isLastSlide = currentIndex === 3;
-    const nextIndex = isLastSlide ? 1 : currentIndex + 1;
+    const isLastSlide = currentIndex === slides.length - 1;
+    const nextIndex = isLastSlide ? 0 : currentIndex + 1;
     setCurrentIndex(nextIndex);
   };
+  const slides = [
+    { backgroundImage: `url('/images/Women/fashion_banner_Edited.jpg')` },
+    { backgroundImage: `url('/images/Men/man_With_Sunglassess_Edited.jpg')` },
+    {
+      backgroundImage: `url('/images/Men/man_redSuit_Rings_Bag_Italy_Edited.jpg')`,
+    },
+  ];
   return (
     <div>
-      <section className="h-96 bg-yellow-900 relative">
-        <div className="w-full h-full bg-cover bg-banner opacity-80" />
+      {/* <section className="h-96 bg-yellow-900 relative">
+        <div className="w-full h-full bg-cover bg-banner1 opacity-80" />
         <div className="absolute w-full top-32 text-center text-gray-50 drop-shadow-2xl">
           <h2 className="text-6xl">Shop With US</h2>
           <p className="text-2xl">Best Products, High Quality</p>
         </div>
-      </section>
+      </section> */}
 
-      <div className="relative max-w-[1400px]  h-[780px] m-auto py-16 px-4 ">
+      <div className="relative max-w-[1920px]  h-[1080px] m-auto py-16 px-4 ">
         <div
-          className={`w-full h-full bg-contain bg-center duration-500 bg-banner${currentIndex}`}
-        ></div>
+          style={{
+            backgroundImage: `${slides[currentIndex].backgroundImage}`,
+          }}
+          className={`bg-banner${currentIndex} w-full h-full bg-cover bg-center duration-500 `}
+        />
         <div className="absolute bottom-10 right-10 flex text-6xl">
           <IoIosArrowRoundBack onClick={prevSlide} className="cursor-pointer" />
           <IoIosArrowRoundForward
@@ -35,10 +45,11 @@ export default function Banner() {
             className="cursor-pointer"
           />
         </div>
+
         <div>
+          <ChangeUiPic onClick={() => setCurrentIndex(0)} />
           <ChangeUiPic onClick={() => setCurrentIndex(1)} />
           <ChangeUiPic onClick={() => setCurrentIndex(2)} />
-          <ChangeUiPic onClick={() => setCurrentIndex(3)} />
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,11 +9,10 @@ module.exports = {
       },
       backgroundImage: {
         banner: `url('../public/images/banner.jpg')`,
-        banner1: `url('../public/images/Women/fashion_banner.jpg')`,
-        banner2: `url('../public/images/Men/man_With_Sunglassess.jpg')`,
-        banner3: `url('../public/images/Men/man_redSuit_Rings_Bag_Italy.jpg')`,
+        banner0: `url('../public/images/Women/fashion_banner_Edited.jpg')`,
+        banner1: `url('../public/images/Men/man_With_Sunglassess_Edited.jpg')`,
+        banner2: `url('../public/images/Men/man_redSuit_Rings_Bag_Italy_Edited.jpg')`,
       },
-
       fontFamily: {
         sans: ["graphik", "helvetica", "sans-serif"],
       },
