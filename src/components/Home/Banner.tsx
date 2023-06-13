c9e4c0cb780f46f8a410f14aa8d889cbaa4ca75d
JKS5 <jokekhanson@gmail.com>
2023-06-13 14:44:59 +0900

    [Deployment]

--- src/components/Home/Banner.tsx
+++ src/components/Home/Banner.tsx
@@ -1,7 +1,7 @@
 import { IoIosArrowRoundBack, IoIosArrowRoundForward } from "react-icons/io";
 import ChangeUiPic from "./ChangeUiPic";
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const slides = [
   { backgroundImage: `url('/images/Banner/fashion_banner_Edited.jpg')` },
